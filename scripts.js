b91fb792136dac867c2ea493d55e40a18da34bef
Frank Merchlewitz <jlimhb@gmail.com>
2021-01-11 14:43:16 -0600

    Made an array of quiz question objects

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// Declare an array of objects with the quiz questions, options, and the correct answer string that we can reference in a function later
+var quiz = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        question: "Arrays in Javascript can be used to store ____.",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        options: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    }
+];
+
