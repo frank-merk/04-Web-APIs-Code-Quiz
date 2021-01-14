47e462468f3c5f3bc253168f947ae8fb838f85a3
Frank Merchlewitz <jlimhb@gmail.com>
2021-01-14 16:19:53 -0600

    Cleaned up some structure, added pseudocode (can't remember if committed earlier)

--- scripts.js
+++ scripts.js
@@ -53,7 +53,7 @@ function createQuizQuestions(qIndex){
         choice.textContent = item;
         questionEl.appendChild(optionsCreate);
         optionsCreate.appendChild(choice);
-        // need to write a checker function that advances here
+        choice.addEventListener("click", (checkAnswer));
         console.log(item);
         
     });
@@ -69,5 +69,21 @@ start.addEventListener("click", function() {
     createQuizQuestions(qIndex);
 });
 
+function checkAnswer(event) {
+    var selection = event.target;
+    var selectionDiv = document.createElement("div");
+    if (selection.matches("li")) {
+        if (selection.textContent === quiz[qIndex].answer) {
+            selectionDiv.textContent = "Correct: The answer is " + quiz[qIndex].answer; 
+            var printThis = selectionDiv.textContent;
+            console.log(printThis);
+        } else {
+                selectionDiv.textContent = "Wrong: The answer is " + quiz[qIndex].answer;
+            }
+        }
+        qIndex++;
+        createQuizQuestions(qIndex);
+}
+
 /* Pseudocode to finish and test:
-The first question is rendered right now. We want to create a condition where if the user clicks on an element, it checks that the text content of that element matches the answer of the corresponding question object. If it does, it puts some text on the page like "Correct, the answer is ___", and if it's wrong, we subtract 10 points from the score and write out "Wrong, the correct answer is ____". Clicking on a choice element does one of those two things, then increments the qIndex and calls the createQuizQuestions function again, but now it will write the next question, and so on (this should also clear the questions div id, but may need to test...i think the way I have it written it will just replace). Then there are two conditions to finish the quiz. Either they complete it, or the timer hits zero, and they are taken to a prompt screen to enter their high score which will right to local storage. They can view it via JSON stringify on a separate page.
+The first question is rendered right now. We want to create a condition where if the user clicks on an element, it checks that the text content of that element matches the answer of the corresponding question object. If it does, it puts some text on the page like "Correct, the answer is ___", and if it's wrong, we subtract 10 points from the score and write out "Wrong, the correct answer is ____". Clicking on a choice element does one of those two things, then increments the qIndex and calls the createQuizQuestions function again, but now it will write the next question, and so on (this should also clear the questions div id, but may need to test...i think the way I have it written it will just replace). Then there are two conditions to finish the quiz. Either they complete it, or the timer hits zero, and they are taken to a prompt screen to enter their high score which will right to local storage. They can view it via JSON stringify on a separate page. */ 
